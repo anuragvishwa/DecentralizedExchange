cbfcc1b9f16b2e2b57db2ff7e92304551caf7d43
Anurag <anuragvishwacs@gmail.com>
2021-05-01 11:57:01 +0530

    Deployed to Rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,23 @@
+require("dotenv").config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*", // Match any network id
+    },
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`
+        );
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
     },
   },
   contracts_directory: "./src/contracts/",
